6b787904bb254b6361e95d5c910bf8f8147d6803
KATARI REVATHI <katari.revathi@gmail.com>
2024-08-06 18:54:47 +0530

    corsn

--- booking/settings.py
+++ booking/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'booking.urls'
